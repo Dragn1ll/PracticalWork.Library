fedfb703b322d440174b51da442553c1f74e50d5
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-26 19:14:59 +0300

    add summary in IReaderRepository

--- src/PracticalWork.Library/Abstractions/Storage/IReaderRepository.cs
+++ src/PracticalWork.Library/Abstractions/Storage/IReaderRepository.cs
@@ -3,13 +3,36 @@ using PracticalWork.Library.Models;
 
 namespace PracticalWork.Library.Abstractions.Storage;
 
+/// <summary>
+/// Репозиторий для Читателей
+/// </summary>
 public interface IReaderRepository
 {
+    /// <summary>
+    /// Создать карточку читателя
+    /// </summary>
+    /// <param name="reader">Читатель</param>
+    /// <returns>Идентификатор читателя</returns>
     Task<Guid> CreateReader(Reader reader);
     
+    /// <summary>
+    /// Получить карточку читателя по идентификатору
+    /// </summary>
+    /// <param name="readerId">Идентификатор читателя</param>
+    /// <returns>Карточка читателя</returns>
     Task<Reader> GetReaderById(Guid readerId);
     
+    /// <summary>
+    /// Получить взятые читателем книги
+    /// </summary>
+    /// <param name="readerId">Идентификатор читателя</param>
+    /// <returns>Список взятых читателем книг</returns>
     Task<IList<BorrowedBookDto>> GetBorrowedBooks(Guid readerId);
     
+    /// <summary>
+    /// Обновить данные читателя
+    /// </summary>
+    /// <param name="readerId">Идентификатор читателя</param>
+    /// <param name="reader">Карточка с обновлёнными данными</param>
     Task UpdateReader(Guid readerId,Reader reader);
 }
