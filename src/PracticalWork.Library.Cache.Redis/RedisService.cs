39eba7d48d0b7ad67346ccba5d6dd16c7e647458
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-27 15:49:35 +0300

    Add simple RedisService

---
+++ src/PracticalWork.Library.Cache.Redis/RedisService.cs
@@ -0,0 +1,34 @@
+using System.Text.Json;
+using Microsoft.Extensions.Caching.Distributed;
+using PracticalWork.Library.Abstractions.Storage;
+
+namespace PracticalWork.Library.Cache.Redis;
+
+/// <inheritdoc cref="IRedisService"/>
+public class RedisService : IRedisService
+{
+    private readonly IDistributedCache _cache;
+
+    public RedisService(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
+    
+    /// <inheritdoc cref="IRedisService.GetAsync{T}"/>
+    public async Task<T> GetAsync<T>(string key)
+    {
+        var value = await _cache.GetStringAsync(key);
+        return value == null ? default : JsonSerializer.Deserialize<T>(value);
+    }
+
+    /// <inheritdoc cref="IRedisService.SetAsync{T}"/>
+    public async Task SetAsync<T>(string key, T value, TimeSpan expiration)
+    {
+        var options = new DistributedCacheEntryOptions
+        {
+            AbsoluteExpirationRelativeToNow = expiration
+        };
+
+        await _cache.SetStringAsync(key, JsonSerializer.Serialize(value), options);
+    }
+}
---
+++ src/PracticalWork.Library/Abstractions/Storage/IRedisService.cs
@@ -0,0 +1,24 @@
+namespace PracticalWork.Library.Abstractions.Storage;
+
+/// <summary>
+/// Сервис по работе с кэшем Redis
+/// </summary>
+public interface IRedisService
+{
+    /// <summary>
+    /// Получить значение из кэша
+    /// </summary>
+    /// <param name="key">Ключ</param>
+    /// <typeparam name="T">Тип возвращаемого значения</typeparam>
+    /// <returns>Значение хранящееся в кэше</returns>
+    Task<T> GetAsync<T>(string key);
+
+    /// <summary>
+    /// Изменить/добавить значение в кэш
+    /// </summary>
+    /// <param name="key">Ключ</param>
+    /// <param name="value">Значение</param>
+    /// <param name="expiration">Время жизни кэша</param>
+    /// <typeparam name="T">Тип отправляемого значения</typeparam>
+    Task SetAsync<T>(string key, T value, TimeSpan expiration);
+}
