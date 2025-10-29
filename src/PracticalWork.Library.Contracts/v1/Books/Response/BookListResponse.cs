95cd380d9b5078707c8effca2117a9d2d51d3d71
Shakhzod Abdulloev <abdulloev-2005@mail.ru>
2025-10-30 00:22:53 +0300

    Add response for bookList

---
+++ src/PracticalWork.Library.Contracts/v1/Books/Response/BookListResponse.cs
@@ -0,0 +1,21 @@
+using PracticalWork.Library.Contracts.v1.Abstracts;
+
+namespace PracticalWork.Library.Contracts.v1.Books.Response;
+
+/// <summary>
+/// Информация о книгах по фильтрам
+/// </summary>
+/// <param name="Id">Идентификатор книги</param>
+/// <param name="Title">Название книги</param>
+/// <param name="Authors">Авторы книги</param>
+/// <param name="Description">Описание книги</param>
+/// <param name="Year">Год выпуска книги</param>
+/// <param name="CoverImagePath">Ссылка на изображение обложки</param>
+public record BookListResponse(
+    Guid Id, 
+    string Title, 
+    IReadOnlyList<string> Authors, 
+    string Description, 
+    int Year, 
+    string CoverImagePath
+    ) : AbstractBook(Title, Authors, Description, Year);
